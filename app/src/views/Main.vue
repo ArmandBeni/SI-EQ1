d3fd061061d66dee675dab524a0d49c39876b3c1
ArmandBeni <armand.benichou@hetic.net>
2019-10-10 11:22:12 +0200

    upd

--- app/src/views/Main.vue
+++ app/src/views/Main.vue
@@ -1,17 +1,41 @@
 <template>
-  <div class="hello">
-    <h1>TIajlekrgbnaleikrjgberjhTRE</h1>
+
+  <div class="main">
+
+    <!-- Alert info -->
+    <div class="alert-info danger">
+      <!-- <img src="../assets/icon-location.png" alt="icon of location"> -->
+      <p>Aucun incident en cours</p>
+    </div>
+
   </div>
+  
+
 </template>
 
 <script>
 export default {
-
+  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+
+.alert-info {
+  background: #F7F7F7;
+  width: 100vw;
+  height: 260px;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+}
+
+.danger {
+  background: #E7464C;
+  color: #ffffff;
+}
+
 h3 {
   margin: 40px 0 0;
 }
